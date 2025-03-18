3588324924d9b978ee65b7a895dbb971ae712c65
HoChoRoo <rttkhc0304@gmail.com>
2025-03-18 17:11:43 +0900

    feat: 헤더파일 생성

---
+++ src/components/layouts/MainHeader.tsx
@@ -0,0 +1,18 @@
+import { navMenuData } from "@/data/initialDatas";
+import React from "react";
+
+export default function MainHeader() {
+  return (
+    <header>
+      <nav className="w-full px-[16px] py-3">
+        <ul className="w-full flex justify-between items-center">
+          {navMenuData.map((menu) => (
+            <li key={menu.id}>
+              <menu.icon className="red" />
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
